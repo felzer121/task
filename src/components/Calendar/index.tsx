8f4e37d51c48c1cd1513239ac30adb03fc502a7a
Felzer <warhammer431@mail.ru>
2021-09-11 14:21:55 +0300

    calendar functional viewing task

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import _map from "lodash/map";
 import _range from 'lodash/range'
 import './style.scss'
 import { ProjectType } from "../SideBarList";
+import { TaskType } from "../../pages/TasksPage/types";
 
 interface CalendarProps {
   today: Date
   project: ProjectType
+  onSelectedCalendarTask: (task: TaskType[]) => void
+  openedTask: TaskType[]
 }
 interface MonthArray {
   month: number
   year: number
 }
 
-export const Calendar = ({today, project}:CalendarProps) => {
+export const Calendar = ({today, project, openedTask, onSelectedCalendarTask}:CalendarProps) => {
   const month = today.getMonth();
   const year = today.getFullYear();
+  const refContainer = useRef(null);
 
   // Массив дней в месяце и месяцов начиная с текущего
   const totalDaysOfMonth:number[] = []
-  const monthArray:MonthArray[] = []
+  const monthArray: MonthArray[] = []
   for (let i = month; i < month + 11; i++) {
     if(i < 12) {
       monthArray.push({month: i, year: year})
@@ -30,8 +34,7 @@ export const Calendar = ({today, project}:CalendarProps) => {
     }
   }
 
-
-  const totalWeeksOfMonth = totalDaysOfMonth.map(days =>  Math.ceil(days / 7))
+  const totalWeeksOfMonth = totalDaysOfMonth.map(days => Math.ceil(days / 7))
 
   const buildClassName = (isPass:any, num:number, indexDoM:number,
                           currentYear:number, currentMonth:number) => {
@@ -47,20 +50,42 @@ export const Calendar = ({today, project}:CalendarProps) => {
   return (
     <div className="days-of-month">
       {totalWeeksOfMonth.map((weeks,index) => {
-        let firstIndex = new Date(monthArray[index].year, monthArray[index].month).getDay();
-        if(new Date(monthArray[index].year, monthArray[index].month + 1).getDay() >= 6 && totalDaysOfMonth[index+1] > 30) {
-          totalWeeksOfMonth[index+1] = 6
-        }
+        const currentMonth = monthArray[index].month
+        const currentYear = monthArray[index].year
+        let isActive = false
+
+        const taskCurrenMonth = project.tasks.filter(task => task.dueOn.toDate().getMonth() === currentMonth)
+        let firstIndex = new Date(currentYear, currentMonth).getDay() === 0 ?
+          7 : new Date(currentYear, currentMonth).getDay()
+
+        if(firstIndex > 5 && totalDaysOfMonth[index] > 30)
+          weeks += 1
+
+        if(firstIndex >= 2 && totalDaysOfMonth[index] === 28)
+          weeks += 1
+
+        if(openedTask[0].dueOn.toDate().getMonth() === currentMonth)
+          isActive = true
+
         let from = 1;
         return(
           <div className='month'>
           {_map(_range(0, weeks * 7), (indexDoM) => {
             const isPass = indexDoM + 1 < firstIndex;
+            let task:TaskType[] = []
+            let isActiveClass = false
+            if(taskCurrenMonth)
+              task = taskCurrenMonth.filter(task => task.dueOn.toDate().getDate() === new Date(currentYear, currentMonth, from).getDate())
+
+            if(isActive)
+              if(openedTask[0].dueOn.toDate().getDate() === new Date(currentYear, currentMonth, from).getDate())
+                isActiveClass = true
             return (
               <React.Fragment key={indexDoM}>
                 {indexDoM % 7 === 0 && <div className="break"></div>}
-                <div className={buildClassName(isPass, from, indexDoM, monthArray[index].year, monthArray[index].month)}>
-                  {isPass ? "" : new Date(monthArray[index].year, monthArray[index].month, from++).getDate()}
+                <div ref={refContainer} className={`${buildClassName(isPass, from, indexDoM, currentYear, currentMonth)} ${isActiveClass ? 'month__active' : ''}`} onClick={() => onSelectedCalendarTask(task)}>
+                  <span className='month__day'>{isPass ? "" : new Date(currentYear, currentMonth, from++).getDate()}</span>
+                  <span className='month__task'>{task ? task.length !== 0 ? `${ task.length } TASK` : '' : ''} </span>
                 </div>
               </React.Fragment>
             );
